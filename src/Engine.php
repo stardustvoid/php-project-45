413b81acb86fd4993c1c4e695305d6b44e4c4438
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2024-03-20 21:48:44 +0700

    Add parameter types

--- src/Engine.php
+++ src/Engine.php
@@ -5,7 +5,7 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function startBrainGame($rules, $data)
+function startBrainGame(string $rules, array $data)
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
